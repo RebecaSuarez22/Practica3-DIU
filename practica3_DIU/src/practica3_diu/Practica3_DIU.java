8830153c3cf54d0e945ddc29dbcd85c0e357e253
RebecaSuarez22 <72138219+RebecaSuarez22@users.noreply.github.com>
2021-10-18 19:42:23 +0100

    Add files via upload

---
+++ practica3_DIU/src/practica3_diu/Practica3_DIU.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practica3_diu;
+
+/**
+ *
+ * @author Rebe
+ */
+public class Practica3_DIU {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Matrices x =new Matrices();
+        x.setVisible(true);
+    }
+    
+}
